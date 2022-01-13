d5b105acdc417db26b6c22668dfd4fc60aec8b99
Alicja <alichr07@gmail.com>
2022-01-13 02:55:29 +0100

    1_3 NaszaKolekcja

--- 1_TypyGeneryczne/KolejkaKolowa.cs
+++ 1_TypyGeneryczne/KolejkaKolowa.cs
@@ -46,7 +46,7 @@
         {
             get
             {
-                return (koniecBufora + 1) % bufor.Length == początekBufora;
+                return (koniecBufora + 1) % bufor.Length == poczatekBufora;
             }
         }
     }
