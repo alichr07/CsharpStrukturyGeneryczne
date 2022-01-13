2e3d22fcd5b80aaaecdd01cb05477e9f455e6f56
Alicja <alichr07@gmail.com>
2022-01-13 02:43:31 +0100

    1_3 NaszaKolekcja

---
+++ 1_TypyGeneryczne/KolejkaKołowa.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_TypyGeneryczne
+{
+    public class KolejkaKołowa
+    {
+        private double[] bufor;
+        private int początekBufora;
+        private int koniecBufora;
+        public KolejkaKołowa() : this (pojemność:5)
+        {
+                
+        }
+        public KolejkaKołowa(int pojemność)
+        {
+            bufor = new double[pojemność + 1];
+            początekBufora = 0;
+            koniecBufora = 0;
+        }
+        public void Zapisz(double wartość)
+        {
+            bufor[koniecBufora] = wartość;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+            if (koniecBufora == początekBufora)
+                początekBufora = (początekBufora + 1) % bufor.Length;
+        }
+        public double Czytaj()
+        {
+            var wynik = bufor[początekBufora];
+            początekBufora = (początekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+        public int Pojemność
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+        public bool JestPusty
+        {
+            get
+            {
+                return koniecBufora == początekBufora;
+            }
+        }
+        public bool JestPełny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == początekBufora;
+            }
+        }
+    }
+}
