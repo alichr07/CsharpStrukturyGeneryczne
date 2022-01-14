94e5d244582afcbbc8aa48c781e1e0936ef21d7e
Alicja <alichr07@gmail.com>
2022-01-14 23:49:34 +0100

    1_4 KolejkaKolowa

--- 1_TypyGeneryczne/Program.cs
+++ 1_TypyGeneryczne/Program.cs
@@ -6,6 +6,25 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscwejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscwejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+            Console.WriteLine("W naszej kolejce jest: ");
+            while (!kolejka.JestPusty)
+            {
+                Console.WriteLine("\t\t" + kolejka.Czytaj());
+            }
         }
     }
 }
