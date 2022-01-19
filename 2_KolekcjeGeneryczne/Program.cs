65d19a411ae24cace7774942e7a46d981daee9c1
Alicja <alichr07@gmail.com>
2022-01-19 03:08:03 +0100

    2_7 LinkedList, Dictionary

--- 2_KolekcjeGeneryczne/Program.cs
+++ 2_KolekcjeGeneryczne/Program.cs
@@ -9,8 +9,62 @@ namespace _2_KolekcjeGeneryczne
         {
             //Kolejka();
             //Stos();
+            //HashSet();
+            //LinkedList();
+            //LinkedList2();
 
-            HashSet();
+            var pracownicy = new Dictionary<string, Pracownik>();
+
+            pracownicy.Add("Nowak", new Pracownik { Nazwisko = "Nowak" });
+            pracownicy.Add("Kowal", new Pracownik { Nazwisko = "Kowal" });
+            pracownicy.Add("Uszak", new Pracownik { Nazwisko = "Uszak" });
+
+            var kowal = pracownicy["Kowal"];
+
+            foreach (var pracownik in pracownicy)
+            {
+                Console.WriteLine("{0} : {1}", pracownik.Key, pracownik.Value.Nazwisko);
+            }
+        }
+
+        private static void LinkedList2()
+        {
+            LinkedList<int> lista = new LinkedList<int>();
+
+            lista.AddFirst(4);
+            lista.AddFirst(5);
+            lista.AddFirst(6);
+
+            var elementPierwszy = lista.First;
+            var elementOstatni = lista.Last;
+
+            lista.AddAfter(elementPierwszy, 10);
+            lista.AddBefore(elementPierwszy, 23);
+
+            var wezel = lista.First;
+
+            while (wezel != null)
+            {
+                Console.WriteLine(wezel.Value);
+                wezel = wezel.Next;
+            }
+        }
+
+        private static void LinkedList()
+        {
+            LinkedList<int> lista = new LinkedList<int>();
+
+            lista.AddFirst(4);
+            lista.AddFirst(5);
+            lista.AddFirst(6);
+            lista.AddLast(1);
+            lista.AddLast(2);
+            lista.AddLast(3);
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
         }
 
         private static void HashSet()
