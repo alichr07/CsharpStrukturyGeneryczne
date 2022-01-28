77b8ea208703fe99dd55ee87dfed258419c76c6d
Alicja <alichr07@gmail.com>
2022-01-28 03:30:30 +0100

    KolejkaExtensions

--- 4_MetodyDelegatyGeneryczne/DuzaKolejka.cs
+++ 4_MetodyDelegatyGeneryczne/DuzaKolejka.cs
@@ -31,18 +31,7 @@ namespace _4_MetodyDelegatyGeneryczne
         {
             kolejka.Enqueue(wartosc);
         }
-        public IEnumerable<Twyjscie> ElementJako<Twyjscie>()
-        {
-            var konwerter = TypeDescriptor.GetConverter(typeof(T));
-
-            foreach (var item in kolejka)
-            {
-                var wynik = konwerter.ConvertTo(item, typeof(Twyjscie));
-                yield return (Twyjscie) wynik;
-            }
-        }
-
-
+        
         public IEnumerator<T> GetEnumerator()
         {
             // return kolejka.GetEnumerator();
