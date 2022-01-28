9de24547db379ba680a14eb3daa6605d318aec86
Alicja <alichr07@gmail.com>
2022-01-28 02:42:02 +0100

    4_MetodyDelegatyGeneryczne

---
+++ 4_MetodyDelegatyGeneryczne/DuzaKolejka.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace _4_MetodyDelegatyGeneryczne
+{
+    public class DuzaKolejka<T> : IKolejka<T>
+    {
+        protected Queue<T> kolejka;
+        public DuzaKolejka()
+        {
+            kolejka = new Queue<T>();
+        }
+
+        public virtual bool JestPelny => throw new System.NotImplementedException();
+
+        public virtual bool JestPusty
+        {
+            get
+            {
+                return kolejka.Count == 0;
+            }
+        }
+
+        public virtual T Czytaj()
+        {
+            return kolejka.Dequeue();
+        }
+
+        public virtual void Zapisz(T wartosc)
+        {
+            kolejka.Enqueue(wartosc);
+        }
+        public IEnumerable<Twyjscie> ElementJako<Twyjscie>()
+        {
+            var konwerter = TypeDescriptor.GetConverter(typeof(T));
+
+            foreach (var item in kolejka)
+            {
+                var wynik = konwerter.ConvertTo(item, typeof(Twyjscie));
+                yield return (Twyjscie) wynik;
+            }
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            // return kolejka.GetEnumerator();
+
+            foreach (var item in kolejka)
+            {
+                // filtrowanie
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return kolejka.GetEnumerator();
+        }
+    }
+}
---
+++ 4_MetodyDelegatyGeneryczne/IKolejka.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace _4_MetodyDelegatyGeneryczne
+{
+    public interface IKolejka<T> : IEnumerable<T>
+    {
+        bool JestPelny { get; }
+        bool JestPusty { get; }
+
+        T Czytaj();
+        void Zapisz(T wartosc);
+
+        IEnumerable<Twyjscie> ElementJako<Twyjscie>();
+    }
+}
